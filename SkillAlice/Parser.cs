eef90e04d74883d1029ce48e9867e5c2a738f5b6
RedOWL <vda.perm@mail.ru>
2019-05-18 13:00:27 +0500

    Added post request to get data

--- SkillAlice/Parser.cs
+++ SkillAlice/Parser.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SkillAlice
 {
-    class Parser
+    class Request
     {
+        public delegate void OnRequestComplete(string data); // Request Complete Callback
+        public delegate void OnRequestError(string code); // Request Error Callback
+
+        internal static async Task<bool> Get(string url, FormUrlEncodedContent data, OnRequestComplete complete, OnRequestError error) 
+        {
+            try
+            { // Creating client and send request
+                // Set client vars
+                var myHttpClient = new HttpClient(); // Create HTTP Client
+                var response = await myHttpClient.PostAsync(url, data); // HTTP Response
+                var json = await response.Content.ReadAsStringAsync(); // JSON Data
+                try
+                { 
+                    complete(json); // Show Complete
+                    return true;
+                }
+                catch (Exception ex)
+                { // Failed to decode data
+                    error(ex.Message); // Show Error
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            { // Failed to send request
+                error(ex.Message); // Show Error
+                return false;
+            }
+        }
+
 
     }
 }
--- SkillAlice/Program.cs
+++ SkillAlice/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -11,7 +12,19 @@ namespace SkillAlice
     {
         static void Main(string[] args)
         {
+            GetData();
+            Console.Read();
+        }
+
+        static async void GetData()
+        {
+            var authContent = new[]{
+                        new KeyValuePair<string, string>("token", Hidden.Token),   // App token
+                    };
             
+            var formContent = new FormUrlEncodedContent(authContent); // Serealize request params
+
+            bool c = await Request.Get(Hidden.Gate, formContent, (s) => { Console.WriteLine(s); }, (e) => { Console.WriteLine(e); });
         }
     }
 }
